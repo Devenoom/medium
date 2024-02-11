1e9439d5ed07eaee78e8e79ef5178e44670f0408
Emwizedes <133639009+Emwizedes@users.noreply.github.com>
2024-02-11 23:58:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const navbarNav = document.querySelector(".navbar-nav");
+
+// document.querySelector("#hamburger-menu").onclick = () => {
+//   navbarNav.classList.toggle("active");
+// };
+
+function toggleMenu() {
+  var navbarNav = document.getElementById("navbar-nav");
+  navbarNav.classList.toggle("show");
+}
+
